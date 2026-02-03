2cb5c7b88ef072318e34c7f2090b83660faa3745
Your Name <ls>
2026-02-03 16:18:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const noBtn = document.querySelector(".no-btn");
+const container = document.querySelector(".container");
+
+noBtn.addEventListener("mouseenter", () => {
+  const containerRect = container.getBoundingClientRect();
+  const btnRect = noBtn.getBoundingClientRect();
+
+  const maxX = containerRect.width - btnRect.width;
+  const maxY = containerRect.height - btnRect.height;
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
